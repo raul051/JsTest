7d084dee36a21843b685d68031de1fcb6103077a
José Raúl <raultamayobriones@gmail.com>
2021-07-03 21:42:31 -0500

    Video 4 arrays

---
+++ _test_/arrays.test.js
@@ -0,0 +1,13 @@
+import { arrayFruits, arraycolors } from "../arrys";
+
+describe('Comprobarque existe un elemento', () => {
+    test('¿Tiene una platano?', () => {
+        expect(arrayFruits()).toContain('platano');
+    });
+    test('No contiene un platano', () => {
+        expect(arrayFruits()).not.toContain('banana');
+    });
+    test('Comprobar el tamaño de un arreglo',() => {
+        expect(arrayFruits()).toHaveLength(5);
+    })
+});
---
+++ arrys.js
@@ -0,0 +1,6 @@
+const fruits = ['platano','melon','sandia','limon','pepinillo'];
+const colors = ['azul','verde','rojo','rosa','amarillo'];
+
+export const arrayFruits = () => fruits;
+export const arraycolors = () => colors;
+
