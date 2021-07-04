48979aecbae1705fe2d70bf1c6cf8fda15bd13dc
José Raúl <raultamayobriones@gmail.com>
2021-07-03 23:33:09 -0500

    C4 V2 Excepciones

--- __test__/snapchot.test.js
+++ __test__/snapchot.test.js
@@ -5,4 +5,23 @@ describe('Es hora de las instantaneas', () => {
     test('Snapchot', () => {
         expect(getCharacter(rick)).toMatchSnapshot();
     });
+
+    test('Fallara la instantanea', () => {
+        const user = {
+            createAt: new Date(),
+            id: Math.floor(Math.random() *20) 
+        }
+        expect(user).toMatchSnapshot();
+    });
+
+    test('Tenemos una excepción', () => {
+        const user = {
+            id: Math.floor(Math.random() *20),
+            name: 'Oscar Barajas'
+        }
+        expect(user).toMatchSnapshot({
+            id: expect.any(Number)
+        })
+    });
+
 });
